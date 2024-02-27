4530355e5af341c4462bead1a59fb67514e3fc76
Amarachi Aka <124574651+Grace-Aka@users.noreply.github.com>
2024-02-27 08:46:44 +0100

    Update print personalize message

--- user_input.py
+++ user_input.py
@@ -8,4 +8,4 @@ age = input("Enter your age: ")
 location = input("Enter your location: ")
 
 # Print personalized message
-print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+print("Hello {}, you are {} years old and you live in {}.".format(name, age, location))
